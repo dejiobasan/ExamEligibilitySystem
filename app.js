67af94deece07537271964fd2073d0425b8021bf
Deji Obasan <dejiobasan@yahoo.com>
2023-02-13 12:43:46 +0100

    landing page success.

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ const port = 3000;
 
 app.set('view engine', 'ejs');
 
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
 app.get("/", function(req, res) {
     res.render("landing");
 });
