46f1df2c75ce89c1bf5d3a560a8bbe37d8573af7
Deji Obasan <dejiobasan@yahoo.com>
2023-02-13 07:23:46 +0100

    Monday corrections

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ const port = 3000;
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
-
 app.get("/", function(req, res) {
     res.render("landing");
 });
