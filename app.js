7e2ac00b30507f66b7b1e0749159912474e363eb
Deji Obasan <dejiobasan@yahoo.com>
2023-03-02 20:44:56 +0100

    integrated the fingerprint, and displays the image to the Enroll page.

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 
@@ -13,6 +16,36 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+const connection1 = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE1
+});
+
+const connection2 = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE2
+});
+
+connection1.connect(function(err) {
+    if (err) {
+      console.error('Error connecting to database 1: ' + err.stack);
+      return;
+    }
+    console.log('Connected to database 1.');
+});
+  
+connection2.connect(function(err) {
+    if (err) {
+      console.error('Error connecting to database 2: ' + err.stack);
+      return;
+    }
+    console.log('Connected to database 2.');
+});
 
 
 app.get("/", function(req, res) {
@@ -33,11 +66,20 @@ app.get("/ExamCheckIn", function(req, res){
 
 
 app.post("/EnrollStudents", function(req, res){
-    const FirstName = ""
+    // const FirstName = ;
+    // const LastName = ;
+    // const MaticNo = ;
+    // const CourseCode = ;
+    // const CourseTitle = ;
+    // const fingerprinttemplate = ;
+
 });
 
 app.post("/EnrollLecturers", function(req, res){
-
+    // const LFirstName = ;
+    // const LLastName = ;
+    // const LCourseCode = ;
+    // const LCourseTitle = ;
 });
 
 app.post("/ExamCheckIn", function(req, res){
--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,40 @@
+const reader = new dp.devices.FingerprintReader()
+const outputContainer = document.querySelector(".fingerprints-container")
+const button = document.querySelector("#EnrollButton")
+const Utf8 = dp.core.Utf8
+const Base64 = dp.core.Base64
 
+reader.on("SamplesAcquired", (e) => {
+    console.log(e)
+    const image = new Image()
+    const sample = e.samples[0]
+    image.src = "data:image/png;base64," + btoa(Utf8.fromBase64Url(sample))
+    outputContainer.appendChild(image)
+});		
+
+reader.on("DeviceConnected", (e) => {
+    console.log(e)
+});
+async function wait(duration) {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve(), duration * 1000)
+    })
+}
+
+button.addEventListener("click", async (e) => {
+    const devices = await reader.enumerateDevices()
+
+    if (devices.length === 0) {
+        throw new Error("No fingerprint reader is connected")
+    }
+
+    const deviceUid = devices[0]
+
+    console.log("starting fingerprint acquisition")
+    await reader.startAcquisition(dp.devices.SampleFormat.PngImage, deviceUid)
+
+    await wait(10)
+
+    console.log("stoping fingerprint acquisition")
+    await reader.stopAcquisition(deviceUid)
+});
